328cc874667e35e1c4c645ec03cb876f3adee8ea
Ryan Tan <ryan@redairship.com>
2025-10-22 09:06:18 +0800

    feat(openai): try a custom prompt

--- lib/openai/client.ts
+++ lib/openai/client.ts
@@ -101,7 +101,20 @@ export async function generateStoryContent(
         {
           role: 'system',
           content:
-            "You are a creative children's story writer specializing in educational adventures. Generate structured story data in JSON format.",
+            "You are a creative children's story writer specializing in short educational adventures that weaves around a list of words. \n\n\n" +
+            'The stories take the form of 15-25 short paragraphs, each 1-2 sentences long, which we will eventually put together to form the story.\n\n\n' +
+            'The first paragraph sets the setting and hero using 2-3 sentences. Incorporate one of the given words here.\n\n\n' +
+            'The second paragraph sets the villain and a problem for the hero. Try to incorporate one of the given words here.\n\n\n' +
+            'The third paragraph explains the mission of the hero - for example to save the villagers, rescue her friends, find the stolen treasures, etc.\n\n\n' +
+            "The next 8 or so paragraphs describes the short adventure of the hero while on the mission. In each paragraph, try to incorporate one of the given words as something the reader has to figure out - don't give it away in the narrative! We'll then let the user play a short game to figure it out.\n\n\n" +
+            "At suitable times, circle back to the word used in the first paragraph. Example, the first paragraph may mention that the hero likes to eat carrots. We can then let the reader play a mini game where they have to guess what the hero likes to eat, in order to overcome one of the in-story challenges - maybe it's a quiz given by a wizard, or a challenge given by a witch.\n\n\n" +
+            'Sometimes the reader would fail the challenges, so we will prepare another 5 obstacles the hero may need to go through - structure these as optional obstacles that we can introduce when required, but does not really affect the story.\n\n\n' +
+            'Next, craft a paragraph where the hero faces the boss villain and up the stakes.\n\n\n' +
+            'For the next few paragraphs, craft a challenge for each of the given words - the reader has to figure out each word in order to advance to the next challenge. For these paragraphs, make them independent of each other, i.e. make sure they work whether they are included in the final story or not. We will pick and chose from these paragraphs when putting together the final story.\n\n\n' +
+            'At the end, craft a challenge for the word used in paragraph 2 in order for the reader to deal the final blow to the villain.\n\n\n' +
+            'Finally, craft a happy ending - to be used if the reader is successful in overcoming the challenge, and a unhappy ending - to be used if the reader fails to overcome the challenge. We will pick either one when putting the final story together.' +
+            '\n\n\nKeep the language age-appropriate for children learning to spell.' +
+            '\n\n\nGenerate structured story data in JSON format.',
         },
         {
           role: 'user',
@@ -160,30 +173,36 @@ export async function generateStoryContent(
  * @returns Formatted prompt string
  */
 function buildPrompt(request: StoryGenerationRequest): string {
-  const { theme, wordList, beatType = 'narrative', context = '' } = request;
+  const {
+    // v
+    theme,
+    wordList,
+    // beatType = 'narrative',
+    // context = '',
+  } = request;
 
   let prompt = `Generate a ${theme}-themed story beat for a children's educational spelling game.\n\n`;
 
-  if (context) {
-    prompt += `Previous story context: ${context}\n\n`;
-  }
+  // if (context) {
+  //   prompt += `Previous story context: ${context}\n\n`;
+  // }
 
   prompt += `Words to practice: ${wordList.join(', ')}\n\n`;
 
-  switch (beatType) {
-    case 'narrative':
-      prompt += 'Create a short narrative paragraph (2-3 sentences) that advances the story.';
-      break;
-    case 'game':
-      prompt += `Create an engaging prompt that introduces a spelling challenge for the word "${wordList[0]}".`;
-      break;
-    case 'choice':
-      prompt +=
-        'Create a story choice with a question and two options for the player to choose from.';
-      break;
-  }
-
-  prompt += '\n\nKeep the language age-appropriate for children learning to spell.';
+  // switch (beatType) {
+  //   case 'narrative':
+  //     prompt += 'Create a short narrative paragraph (2-3 sentences) that advances the story.';
+  //     break;
+  //   case 'game':
+  //     prompt += `Create an engaging prompt that introduces a spelling challenge for the word "${wordList[0]}".`;
+  //     break;
+  //   case 'choice':
+  //     prompt +=
+  //       'Create a story choice with a question and two options for the player to choose from.';
+  //     break;
+  // }
+  //
+  // prompt += '\n\nKeep the language age-appropriate for children learning to spell.';
 
   return prompt;
 }
