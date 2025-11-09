b2251fd7185a6a57eac3098f72da1fbf8ba8f7d5
Ryan Tan <ryan@redairship.com>
2025-11-09 22:34:45 +0800

    feat: remove unused

--- machines/story-progress/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Story Progress Machine
- *
- * Exports for the story progression state machine
- */
-
-export { storyProgressMachine, initializeStoryProgress } from './storyProgressMachine';
-export type { StoryProgressContext, StoryProgressEvent } from './types';
